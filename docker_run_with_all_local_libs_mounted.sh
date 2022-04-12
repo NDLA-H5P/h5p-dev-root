186236b55deaeff337355d47f865dcc28973c4bc
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-04-12 07:32:42 +0200

    add h5p-editor-copyright to topic map and timeline

--- docker_run_with_all_local_libs_mounted.sh
+++ docker_run_with_all_local_libs_mounted.sh
@@ -7,6 +7,8 @@ do
     mounts="${mounts} -v ${PWD}/${lib}:/var/www/html/sites/default/files/h5p/development/${lib}"
 done
 
+[ -d font-awesome/ ] && mounts="${mounts} -v ${PWD}/font-awesome:/var/www/html/sites/default/files/h5p/development/font-awesome"
+
 echo $mounts
 
 docker run --rm -d -p $2:80 $mounts $1
